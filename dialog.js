9a237fc15392e163fca8d05ddb0a2fee3b4aa695
Philippe Dionne <dionne.phil@gmail.com>
2016-09-15 11:00:34 -0400

    Add uuid helper function

--- dialog.js
+++ dialog.js
@@ -1,5 +1,11 @@
 let request = require('request');
 
+function uuid() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+    return v.toString(16);
+  });
+};
 
 // Example interface to facilitate sending messages to the Dialog API.
 let Dialog = {
